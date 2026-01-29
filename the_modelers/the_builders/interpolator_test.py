61ede647e18f7e7cf2334c4c1a1e6c282ce832e4
Haygen Warren <haygen@ad.unc.edu>
2026-01-29 12:02:39 -0500

    testing interpolator

--- the_modelers/the_builders/interpolator_test.py
+++ the_modelers/the_builders/interpolator_test.py
@@ -1,7 +1,7 @@
 from the_interpolator import get_model_mag, brute_force_likelihood
 
-observed_mags = {"G": 8.83, "BP": 9.79, "RP": 7.89}
-observed_errs = {"G": 0.02, "BP": 0.03, "RP": 0.02}
+observed_mags = {"g": 16.4520000, "r": 14.8454000, "i": 13.668100, "z": 12.6126000}
+observed_errs = {"f": 0.0040676, "r": 0.0037485, "i": 0.000602, "z": 0.0039562}
 
 mass, age, feh = brute_force_likelihood(observed_mags, observed_errs)
 print(f"Brute-force likelihood results: mass={mass}, age={age}, feh={feh}")
@@ -9,10 +9,10 @@ print(f"Brute-force likelihood results: mass={mass}, age={age}, feh={feh}")
 model_mags = get_model_mag(mass, age, feh)
 print("Model magnitudes at best-fit parameters:", model_mags)
 
-master_list = {"G": 8.80, "BP": 9.75, "RP": 7.92}
-comparison = {
-    band: model_mags[band] - master_list[band]
-    for band in master_list
-    if band in model_mags
-}
-print("Model - master list differences:", comparison)
+# master_list = {"G": 8.80, "BP": 9.75, "RP": 7.92}
+# comparison = {
+#     band: model_mags[band] - master_list[band]
+#     for band in master_list
+#     if band in model_mags
+# }
+# print("Model - master list differences:", comparison)
