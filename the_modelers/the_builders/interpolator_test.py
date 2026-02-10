77e0ab98e957f11b5068ae446c58444b53410674
Haygen Warren <haygen@ad.unc.edu>
2026-02-10 12:15:22 -0500

    testing interpolator

--- the_modelers/the_builders/interpolator_test.py
+++ the_modelers/the_builders/interpolator_test.py
@@ -1,10 +1,14 @@
 from the_interpolator import get_model_mag, brute_force_likelihood
 
-observed_mags = {"g": 10.74, "r": 9.13, "i": 7.96, "z": 6.90}
-observed_errs = {"g": 0.005, "r": 0.005, "i": 0.005, "z": 0.005}
+# observed_mags = {"g": 10.74, "r": 9.13, "i": 7.96, "z": 6.90}
+# observed_errs = {"g": 0.005, "r": 0.005, "i": 0.005, "z": 0.005}
+#
+# mass, age, feh = brute_force_likelihood(observed_mags, observed_errs)
+# print(f"Brute-force likelihood results: mass={mass}, age={age}, feh={feh}")
 
-mass, age, feh = brute_force_likelihood(observed_mags, observed_errs)
-print(f"Brute-force likelihood results: mass={mass}, age={age}, feh={feh}")
+mass = 0.56
+age = 9300000
+feh = 0.0
 
 model_mags = get_model_mag(mass, age, feh)
 print("Model magnitudes at best-fit parameters:", model_mags)
